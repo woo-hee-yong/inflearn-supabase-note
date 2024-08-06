32852bba46289de3d88255c93d0e6e11294b432d
우희용 <kangtao@nate.com>
2024-08-06 16:30:03 +0900

    supabase 추가

--- app/ui.tsx
+++ app/ui.tsx
@@ -5,25 +5,29 @@ import  Sidebar from "@/components/sidebar";
 import  NewNote from "@/components/new-note";
 import  NoteViewer from "@/components/note-viewer";
 import  EmptyNote from "@/components/empty-note";
-import  {useState} from 'react';
+import  {useState, useEffect} from 'react';
+import { supabase } from "@/utils/supabase";
+import { Database } from "@/types_db";
 
-const notes = [
-    {
-        id: 1,
-        title: "노트 1",
-        content: "노트 내용입니다. 1"
-    },
-    {
-        id: 2,
-        title: "노트 2",
-        content: "노트 내용입니다. 2"
-    }
-
-]
 
 export default function UI(){
     const [activeNoteId, setActiveNoteId] = useState(null);
     const [isCreating, setIsCreating] = useState(false);
+    const [notes, setNotes] = useState<Database['public']['Tables']['note']['Row'][]>([]);
+
+    const fetchNotes = async () => {
+      const {data, error} = await supabase.from('note').select("*");
+      if(error){
+        alert(error.message);
+        return false;
+      }
+      setNotes(data);
+    }
+
+    useEffect(() => {
+      fetchNotes();
+    }, []);
+
     return (
         <main className="w-full h-screen flex flex-col">
           <Header />
