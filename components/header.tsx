c07d2ed31a4fc56d61ff754c873d358ba2f7a238
우희용 <kangtao@nate.com>
2024-08-19 15:07:49 +0900

    layout 변경

--- components/header.tsx
+++ components/header.tsx
@@ -12,27 +12,30 @@ export default function Header({
             <div className="w-full bg-green-600 py-3 px-4 bg-stone-600">
                 <a href="./" className="font-bold text-white text-3xl">⚾ Hee Dragon World</a>
             </div>
-            {activeNewNoteBtn ? (
-                <div className="pt-2">
-                    <button 
-                        onClick={()=> {
-                            setActiveNoteId(null)
-                            setIsCreating(true)
-                        }}
-                        className="p-2 text-lg font-bold border border-gray-600 rounded-lg w-full">
-                        ➕ 새로운 노트
-                    </button>
-                </div>
-            ):(
-              <></>
-            )} 
-              <input 
-                    type="text" 
-                    value={search}
-                    onChange = {(e) => setSearch(e.target.value)}
-                    placeholder = "노트를 검색해보세요."
-                    className="w-full p-2 border rounded-md border-gray-600 mt-2"
-                />
+            <div className="m-2">
+                {activeNewNoteBtn ? (
+                    <div className="w-full justify-between flex">
+                        <input 
+                            type="text" 
+                            value={search}
+                            onChange = {(e) => setSearch(e.target.value)}
+                            placeholder = "노트를 검색해보세요."
+                            className="p-2 text-lg font-bold border border-gray-600 rounded-lg w-full"
+                        />
+                        <button 
+                            onClick={()=> {
+                                setActiveNoteId(null)
+                                setIsCreating(true)
+                            }}
+                            className="p-2 ml-2 text-lg font-bold border border-gray-600 rounded-lg">
+                            ➕
+                        </button>
+                    </div>
+                ):(
+                    <></>
+                )} 
+            </div>
+                
         </header>
     ;
 }
