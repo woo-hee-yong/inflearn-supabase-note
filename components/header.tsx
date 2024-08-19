606d6ed7394572de425147fde48c0cf20f7a86f4
우희용 <kangtao@nate.com>
2024-08-19 11:00:13 +0900

    첫 타이틀 수정

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@
 
 export default function Header(){
     return <header className="w-full bg-green-600 py-3 px-4 bg-stone-600" >
-            <img src="/supanote-logo.png" alt="Supanote Logo" className="h-7"/>
+            <span className="font-bold text-white text-3xl">⚾ Hee Dragon World</span>
         </header>
     ;
 }
